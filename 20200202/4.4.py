81f5d1d9fe912647d05f8d1d6a939d71d4223c9d
luch <1097650398@qq.com>
2020-02-14 22:34:08 +0800

    根据老师的答案修改第四次作业

--- 20200202/4.4.py
+++ 20200202/4.4.py
@@ -13,10 +13,25 @@ def shuffle(x, y):
     y = [y[i] for i in l]
     return x, y
 
+# 老师的shuffle
+def shuffle_laoshi(x, y):
+    xy = list(zip(x, y))
+    # print(xy)
+    # 指定随机的种子，指定种子后，每次随机的结果都一样
+    random.seed(1)
+    random.shuffle(xy)
+    # print(*xy)
+    # print(zip(*xy))
+    x, y = zip(*xy)
+    return x, y
+
 
 def main():
     x = ['快递太慢了！', '不好吃', '特别难吃', '要齁死我了', '很划算', '下次还来', '味道很不错！', '香']
     y = ['差评', '差评', '差评', '差评', '好评', '好评', '好评', '好评']
+    # x_laoshi, y_laoshi = shuffle_laoshi(x, y)
+    # print(x_laoshi)
+    # print(y_laoshi)
     x, y = shuffle(x, y)
 
     # print result for certify
