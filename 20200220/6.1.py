334af2400d8f24188423b4bc24b0e1a8d98ab538
luch <1097650398@qq.com>
2020-02-22 13:46:21 +0800

    完成第6次作业

---
+++ 20200220/6.1.py
@@ -0,0 +1,84 @@
+#  爬虫数据集筛选及保存
+
+import requests
+from bs4 import BeautifulSoup
+
+# 以前遇到过的函数
+
+def build_url(city_coding, year=None, month=None):
+    """
+    创建网页链接
+    paramters:
+        city_coding: 城市名称(英文)
+        year: 年份
+        month: 月份
+    return:
+        url: 可访问的链接
+    """
+    BASE = 'http://www.tianqihoubao.com/aqi/'
+    city_base_url = BASE + '{}.html'
+    city_date_base_url = BASE + '{}-{}{}.html'
+    
+    if year is not None and month is not None:
+        month = str(month) if month >= 10 else '0' + str(month)
+        return city_date_base_url.format(city_coding, year, month)
+    else:
+        return city_base_url.format(city_coding)
+
+
+def parse(url, city_name):
+    """
+    抓取网页信息
+    parameters:
+        url: 需要抓取的网页链接
+        city_name: 城市名称(用于数据标识)
+    returns:
+        result: 抓取的信息
+    """
+    response = requests.get(url)
+    if response.ok:
+        html = response.text
+        
+        soup = BeautifulSoup(html, features='html')
+        data_table = soup.table
+        
+        content = data_table.contents
+        
+        result = []
+        for index, c in enumerate(content[1::2]):
+                if index == 0:
+                    result.append(tuple(['城市'] + c.text.split()))
+                else:
+                    result.append(tuple([city_name] + c.text.split()))
+        return result
+    
+    else:
+        if response.status_code == 403:
+            print('403 Forbidden! 抓取太快你被拉黑啦~')
+
+            
+def save(data, file):
+    # 完成数据保存到文件
+    # 使用open函数打开文件，w表示写文件并覆盖原文件内容，设置字符编码utf-8防止中文乱码
+    with open(file, 'w', encoding='utf-8') as f:
+        # 将数据分割成，每个月份的数据占一行，每一行的数据使用,隔开的字符串
+        txtdata = '\n'.join(list(map(lambda item: ','.join(item), data)))
+        # 将数据写入文件
+        f.write(txtdata)
+    print('data saved in ', file)
+    
+
+if __name__ == '__main__':
+    datas = []
+    for i in range(1, 2):
+        url = build_url('hangzhou', 2019, i)
+        data = parse(url, '杭州')
+        datas.extend(data)
+    # print(datas)
+    
+    # 只保留质量等级优 良 数据
+    # your code here
+    # 提示：用什么方法对数据进行筛选？
+    
+    # 保存数据
+    save(data, './data.txt')
---
+++ 20200220/6.2.py
@@ -0,0 +1,91 @@
+# 西瓜数据保存和读取
+"""
+添加编号列，并将数据集写入到machine_learning.csv文件，使用pandas读取验证文件是否有效(无错即可)。
+添加一条记录，青绿 硬挺 浊响 稍糊 平坦 硬滑 0.666 0.111 好
+再使用普通文件读取将数据集读取出来，列名读取到columns，数据(带编号)读取到datalist
+在所有数据中过滤出色泽='浅白'的数据
+在所有数据中过滤出密度大于0.5的数据
+"""
+
+import pandas as pd
+
+dataset = \
+"""色泽 根蒂 敲声 纹理 脐部 触感 密度 含糖率 好瓜
+青绿 蜷缩 浊响 清晰 凹陷 硬滑 0.697 0.460 是
+乌黑 蜷缩 沉闷 清晰 凹陷 硬滑 0.774 0.376 是
+乌黑 蜷缩 浊响 清晰 凹陷 硬滑 0.634 0.264 是
+青绿 蜷缩 沉闷 清晰 凹陷 硬滑 0.608 0.318 是
+浅白 蜷缩 浊响 清晰 凹陷 硬滑 0.556 0.215 是
+青绿 稍蜷 浊响 清晰 稍凹 软粘 0.403 0.237 是
+乌黑 稍蜷 浊响 稍糊 稍凹 软粘 0.481 0.149 是
+乌黑 稍蜷 浊响 清晰 稍凹 硬滑 0.437 0.211 是
+乌黑 稍蜷 沉闷 稍糊 稍凹 硬滑 0.666 0.091 否
+青绿 硬挺 清脆 清晰 平坦 软粘 0.243 0.267 否
+浅白 硬挺 清脆 模糊 平坦 硬滑 0.245 0.057 否
+浅白 蜷缩 浊响 模糊 平坦 软粘 0.343 0.099 否
+青绿 稍蜷 浊响 稍糊 凹陷 硬滑 0.639 0.161 否
+浅白 稍蜷 沉闷 稍糊 凹陷 硬滑 0.657 0.198 否
+乌黑 稍蜷 浊响 清晰 稍凹 软粘 0.360 0.370 否
+浅白 蜷缩 浊响 模糊 平坦 硬滑 0.593 0.042 否
+青绿 蜷缩 沉闷 稍糊 稍凹 硬滑 0.719 0.103 否"""
+
+# 将数据写入csv文件
+# your code here 
+file = r'machine_learning.csv' # 文件名称，学员可修改或不修改
+# 将数据转化为列表
+dataset = list(map(lambda item: item.split(' '), dataset.split('\n')))
+# 循环列表，添加上编号列
+for i in range(len(dataset)):
+    if i == 0:
+        dataset[i].insert(0, '编号')
+    else:
+        dataset[i].insert(0, '{}'.format(i))
+# 将新加入的数据转换为字符串
+txtdata = '\n'.join([' '.join(item) for item in dataset])
+# 写入数据
+with open(file, 'w', encoding='utf-8') as f:
+    f.write(txtdata)
+
+
+# 向csv文件中加入一条新的数据（数据已给出）
+# your code here
+# 注意每一行数据的间隔符号是什么
+inser_data = '18 青绿 硬挺 浊响 稍糊 平坦 硬滑 0.666 0.111 是'
+# 使用a，表示追加数据，不改变原有数据
+with open(file, 'a', encoding='utf-8') as f:
+    f.write('\n' + inser_data)
+
+print('查看全体数据', '*' * 20)
+# 查看全体数据
+df = pd.read_csv(file)
+print(df.head())
+
+
+# 读取文件存储的数据
+# your code here
+# columns是指列标签
+# datalist指全体数据内容，每一行数据应为一个列表
+columns = []
+datalist = []
+with open(file, 'r', encoding='utf-8') as f:
+    txtdata = f.read()
+    datas = list(map(lambda row: row.split(' '), txtdata.split('\n')))
+    columns = datas[0]
+    datalist = datas[1:]
+
+
+print('验证数据是否相符', '*' * 20)
+# 验证数据信息是否相符
+print(columns==['编号', '色泽', '根蒂', '敲声', '纹理', '脐部', '触感', '密度', '含糖率', '好瓜'])
+print(datalist[-1]==['18', '青绿', '硬挺', '浊响', '稍糊', '平坦', '硬滑', '0.666', '0.111', '是'])
+
+# 在所有数据中过滤出色泽='浅白'的数据
+# 在所有数据中过滤出密度大于0.5的数据
+# your code here
+datalist1 = list(filter(lambda row: row[1] == '浅白', datalist))
+datalist2 = list(filter(lambda row: float(row[7]) > 0.5, datalist))
+
+print('过滤浅白数据', '*' * 20)
+print(datalist1)
+print('过滤密度大于0.5的数据', '*' * 20)
+print(datalist2)
